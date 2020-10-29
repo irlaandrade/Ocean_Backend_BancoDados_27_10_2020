5082f4ee0581f487d04a850dabc825b99f78e8f5
irlaandrade <paula.irla@gmail.com>
2020-10-29 08:56:30 -0300

    Atualização dos endpoints após implementação do MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+const ObjectId = mongodb.ObjectId;
+
+(async () => {
+
+const connectionString = 'mongodb://localhost:27017';
+
+console.info('Conectando ao banco de dados MongoDB...');
+
+const options = {
+    useUnifiedTopology: true
+};
+
+const client = await mongodb.MongoClient.connect(connectionString, options);
+
+console.info('MongoDB conectado com sucesso!');
+
 const app = express();
 
 const port = 3000;
@@ -41,60 +58,62 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-const mensagens = [
-    {
-        id: 1,
-        texto: 'Essa é uma mensagem'
-    },
-    {
-        id: 2,
-        texto: 'Essa é outra mensagem'
-    }
-];
+const db = client.db('ocean_backend_27_10_2020');
+const mensagens = db.collection('mensagens');
 
 // Read All
-app.get('/mensagem', function (req, res) {
-    res.send(mensagens.filter(Boolean));
+app.get('/mensagem', async function (req, res) {
+    const findResult = await mensagens.find({}).toArray();
+    res.send(findResult);
 });
 
 // Create
-app.post('/mensagem', function (req, res) {
+app.post('/mensagem', async function (req, res) {
     const texto = req.body.texto;
 
     const mensagem = {
-        'id': mensagens.length + 1,
         'texto': texto
     };
 
-    mensagens.push(mensagem);
+    const resultado = await mensagens.insertOne(mensagem);
 
-    res.send(mensagem);
+    const objetoInserido = resultado.ops[0];
+
+    res.send(objetoInserido);
 });
 
 // Read Single
-app.get('/mensagem/:id', function (req, res) {
+app.get('/mensagem/:id', async function (req, res) {
     const id = req.params.id;
 
-    const mensagem = mensagens[id - 1];
+    const mensagem = await mensagens.findOne({ _id: ObjectId(id) });
 
     res.send(mensagem);
 });
 
 // Update
-app.put('/mensagem/:id', function (req, res) {
+app.put('/mensagem/:id', async function (req, res) {
     const id = req.params.id;
     const texto = req.body.texto;
 
-    mensagens[id - 1].texto = texto;
+    const mensagem = {
+        _id: ObjectId(id),
+        texto
+    };
 
-    res.send(mensagens[id - 1]);
+    await mensagens.updateOne(
+        { _id: ObjectId(id) },
+        { $set: mensagem }
+    );
+
+    res.send(mensagem);
 });
 
 // Delete
-app.delete('/mensagem/:id', function (req, res) {
+app.delete('/mensagem/:id', async function (req, res) {
     const id = req.params.id;
 
-    delete mensagens[id - 1];
+    await mensagens.deleteOne({ _id: ObjectId(id) });
 
     res.send(`A mensagem de ID ${id} foi removida com sucesso.`);
 });
@@ -102,3 +121,5 @@ app.delete('/mensagem/:id', function (req, res) {
 app.listen(port, function () {
     console.info('App rodando em http://localhost:' + port);
 });
+
+})();
